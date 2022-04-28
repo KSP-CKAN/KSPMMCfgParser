1c4a28a85284c55877bfe22395a0676f1b28e53e
Paul Hebble <pjhebble@gmail.com>
2022-04-28 19:34:16 +0000

    Remove "At AbortIfEntered" from output

--- KSPMMCfgParser/Parser.Extensions.cs
+++ KSPMMCfgParser/Parser.Extensions.cs
@@ -17,5 +17,34 @@ namespace KSPMMCfgParser
         /// </summary>
         public static Parser<TToken, T?> AsNullable<TToken, T>(this Parser<TToken, T> parser)
             where T : class => Optional(parser.Map(v => (T?)v), null);
+
+        /// <summary>
+        /// Borrowed from internal ParsecSharp.ClosureOptimization.
+        /// I don't understand what it does, but it's part of the
+        /// implementation of AbortIfEntered.
+        /// </summary>
+        /// <param name="value">A value</param>
+        /// <param name="_">Some parser state to be ignored</param>
+        /// <returns>
+        /// Parser output object representing the value
+        /// </returns>
+        public static T Const<T, TIgnore>(this T value, TIgnore _)
+            where T : class
+            => value;
+
+        /// <summary>
+        /// A clone of AbortIfEntered that doesn't add junk to its output.
+        /// No user benefits from seeing "At AbortIfEntered -&gt;".
+        /// </summary>
+        /// <param name="parser">A parser that might fail</param>
+        /// <returns>
+        /// Parser output object with error message if parsing fails
+        /// </returns>
+        public static Parser<TToken, T> AbortQuietlyIfEntered<TToken, T>(this Parser<TToken, T> parser)
+            => parser.Alternative(failure =>
+                GetPosition<TToken>().Bind(position =>
+                    position.Equals(failure.State.Position)
+                        ? Fail<TToken, T>(failure.Message)
+                        : Abort<TToken, T>(failure.Message.Const)));
     }
 }
