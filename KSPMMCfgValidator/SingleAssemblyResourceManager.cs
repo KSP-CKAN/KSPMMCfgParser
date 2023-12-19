fa867b0c7ca824226aa33ef289260481be556044
Paul Hebble <pjhebble@gmail.com>
2023-12-18 19:11:41 -0600

    Multi-target net48 and net7.0, streamline Docker image

--- KSPMMCfgValidator/SingleAssemblyResourceManager.cs
+++ KSPMMCfgValidator/SingleAssemblyResourceManager.cs
@@ -1,7 +1,6 @@
 using System.IO;
 using System.Globalization;
 using System.Resources;
-using System.Collections;
 using System.Reflection;
 using System.Collections.Generic;
 
@@ -9,33 +8,49 @@ namespace KSPMMCfgValidator
 {
     // Thanks and credit to this guy: https://stackoverflow.com/q/1952638/2422988
 
-    class SingleAssemblyResourceManager : ResourceManager
+    /// <summary>
+    /// Wrapper around ResourceManager that retrieves strings from the assembly
+    /// rather than external files
+    /// </summary>
+    public class SingleAssemblyResourceManager : ResourceManager
     {
-        public SingleAssemblyResourceManager(string basename, Assembly assembly) : base(basename, assembly)
+        /// <summary>
+        /// Initialize the resource manager
+        /// </summary>
+        /// <param name="basename">To be passed to ResourceManager</param>
+        /// <param name="assembly">To be passed to ResourceManager</param>
+        public SingleAssemblyResourceManager(string basename, Assembly assembly)
+            : base(basename, assembly)
         {
         }
 
-        protected override ResourceSet InternalGetResourceSet(CultureInfo culture,
+        /// <summary>
+        /// Provides resources from the assembly to ResourceManager
+        /// </summary>
+        /// <param name="culture">The language to get</param>
+        /// <param name="createIfNotExists">Set to false to avoid loading if not already cached</param>
+        /// <param name="tryParents">Just gets passed to base class implementation</param>
+        /// <returns></returns>
+        protected override ResourceSet? InternalGetResourceSet(CultureInfo culture,
             bool createIfNotExists, bool tryParents)
         {
-            ResourceSet rs;
-            if (!myResourceSets.TryGetValue(culture, out rs))
+            if (!myResourceSets.TryGetValue(culture, out ResourceSet? rs))
             {
                 // Lazy-load default language (without caring about duplicate assignment in race conditions, no harm done)
-                if (neutralResourcesCulture == null)
+                if (MainAssembly != null)
                 {
-                    neutralResourcesCulture = GetNeutralResourcesLanguage(this.MainAssembly);
+                    neutralResourcesCulture ??= GetNeutralResourcesLanguage(MainAssembly);
                 }
 
                 // If we're asking for the default language, then ask for the
                 // invariant (non-specific) resources.
-                if (neutralResourcesCulture.Equals(culture))
+                if (neutralResourcesCulture?.Equals(culture) ?? true)
                 {
                     culture = CultureInfo.InvariantCulture;
                 }
                 string resourceFileName = GetResourceFileName(culture);
 
-                Stream store = this.MainAssembly.GetManifestResourceStream(resourceFileName);
+                Stream? store = MainAssembly?.GetManifestResourceStream(resourceFileName);
 
                 // If we found the appropriate resources in the local assembly
                 if (store != null)
@@ -53,6 +68,6 @@ namespace KSPMMCfgValidator
         }
 
         private CultureInfo? neutralResourcesCulture;
-        private Dictionary<CultureInfo, ResourceSet> myResourceSets = new Dictionary<CultureInfo, ResourceSet>();
+        private readonly Dictionary<CultureInfo, ResourceSet> myResourceSets = new Dictionary<CultureInfo, ResourceSet>();
     }
 }
